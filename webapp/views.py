e387aa2bdf903f72b20e6a0be1310e350a4436d9
awaciern <awaciern@ncsu.edu>
2020-01-02 01:14:22 -0500

    HTML for board position setup

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from webapp.models import *
 
 # Create your views here.
+def display(request):
+    # Create a new board for this game
+    board = Board.objects.create()
+    board.generate_words()
+    print(board.word.all())
+
+    return render(request, 'board.html', {'words': board.word.all()})
